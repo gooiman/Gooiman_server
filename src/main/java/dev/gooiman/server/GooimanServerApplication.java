0ce042095562940c4ca4ba07bb39092b134170d4
EATSTEAK <me@eatsteak.dev>
2024-09-26 22:50:44 +0900

    init: Spring Starter를 이용한 프로젝트 생성

---
+++ src/main/java/dev/gooiman/server/GooimanServerApplication.java
@@ -0,0 +1,13 @@
+package dev.gooiman.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooimanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooimanServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gooiman/server/GooimanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gooiman.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooimanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

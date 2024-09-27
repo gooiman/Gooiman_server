15ff9c372596e86e3e23143674618d4243df0f6c
koosco <rnxogud123@naver.com>
2024-09-28 02:36:27 +0900

    setting: cors config 추가
    * WebMvcConfig에 cors 설정을 했습니다
    * 추후 security 의존성 추가를 했을 때 사용할 CorsConfig 설정도 함께
      추가했습니다

---
+++ src/main/java/dev/gooiman/server/config/web/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package dev.gooiman.server.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowCredentials(true)
+            .allowedOrigins("http://localhost:3000", "http://localhost:3000")
+            .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*");
+    }
+}

489c8d9ce22bbb89eb03a1475455d04d5309536d
koosco <rnxogud123@naver.com>
2024-09-28 02:38:09 +0900

    setting: audit config 추가
    * jpa audit 사용을 위해 설정을 추가하였습니다
    * 마지막 수정 시간 저장을 위한 base time entity 추가

---
+++ src/main/java/dev/gooiman/server/common/repository/entity/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package dev.gooiman.server.common.repository.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @LastModifiedDate
+    @Column(name = "last_updated")
+    private LocalDateTime lastUpdated;
+}
+

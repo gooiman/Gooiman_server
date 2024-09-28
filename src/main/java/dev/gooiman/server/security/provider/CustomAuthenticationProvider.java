3145b2b3390432b44d30c26d71e22ede4b114b16
koosco <rnxogud123@naver.com>
2024-09-28 22:11:59 +0900

    feat: 로그인 추가
    * AuthenticationController : 로그인 요청 controller
    * CustomAuthenticationProvider : 로그인 요청 검증
    * CustomAuthenticationService : 로그인 요청 처리
      * 로그인 정보가 있는 유저 : 로그인
      * 로그인 정보가 있지만 비밀번호 틀림 : 로그인 실패
      * 로그인 정보가 없는 유저 : 회원가입

---
+++ src/main/java/dev/gooiman/server/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package dev.gooiman.server.security.provider;
+
+
+import static dev.gooiman.server.common.exception.ErrorCode.FAILURE_LOGIN;
+
+import dev.gooiman.server.common.exception.CommonException;
+import dev.gooiman.server.security.application.CustomUserDetailsService;
+import dev.gooiman.server.security.repository.entity.CustomUserDetails;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public Optional<Authentication> authenticate(UUID pageId, Authentication authentication) {
+        String name = (String) authentication.getPrincipal();
+        String password = (String) authentication.getCredentials();
+
+        Optional<CustomUserDetails> userDetail = customUserDetailsService.loadUserByUsername(
+            pageId, name);
+
+        if (userDetail.isEmpty()) {
+            return Optional.empty();
+        }
+
+        userDetail.ifPresent(detail -> validatePassword(password, detail));
+        return Optional.of(new UsernamePasswordAuthenticationToken(userDetail.get(), null,
+            List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+    }
+
+    private void validatePassword(String password, CustomUserDetails userDetails) {
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new CommonException(FAILURE_LOGIN);
+        }
+    }
+}

ac1a13d39d2bb1639db2da283d2a08663a830a82
Hyomin Koo <me@eatsteak.dev>
2024-09-29 01:01:01 +0900

    refactor: memoSummaries에 함수형 mapping 적용

--- src/main/java/dev/gooiman/server/page/application/PageService.java
+++ src/main/java/dev/gooiman/server/page/application/PageService.java
@@ -5,14 +5,14 @@ import dev.gooiman.server.common.exception.CommonException;
 import dev.gooiman.server.common.exception.ErrorCode;
 import dev.gooiman.server.memo.application.dto.MemoSummariesResponseDto;
 import dev.gooiman.server.memo.repository.MemoRepository;
+import dev.gooiman.server.memo.repository.view.MemoSummariesView;
 import dev.gooiman.server.page.application.dto.CreatePageRequestDto;
 import dev.gooiman.server.page.repository.PageRepository;
 import dev.gooiman.server.page.repository.entity.Page;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,37 +21,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PageService {
 
-    private final PageRepository pageRepository;
-    private final MemoRepository memoRepository;
+  private final PageRepository pageRepository;
+  private final MemoRepository memoRepository;
 
-    public Page getPageById(UUID id) {
-        return pageRepository.findById(id)
-            .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_PAGE));
-    }
+  public Page getPageById(UUID id) {
+    return pageRepository.findById(id)
+        .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_PAGE));
+  }
 
-    @Transactional
-    public CommonIdResponseDto create(CreatePageRequestDto dto) {
-        Page page = new Page(dto.name());
-        Page savedPage = pageRepository.save(page);
-        return new CommonIdResponseDto(savedPage.getPageId());
-    }
+  @Transactional
+  public CommonIdResponseDto create(CreatePageRequestDto dto) {
+    Page page = new Page(dto.name());
+    Page savedPage = pageRepository.save(page);
+    return new CommonIdResponseDto(savedPage.getPageId());
+  }
 
-    public MemoSummariesResponseDto.Res memoSummaries(UUID pageId) {
-        Page page = getPageById(pageId);
-        String name = page.getPageName();
+  public MemoSummariesResponseDto memoSummaries(UUID pageId) {
+    Page page = getPageById(pageId);
+    String name = page.getPageName();
 
-        Map<String, Map<String, List<String>>> memoSummaries = new HashMap<>();
-        List<Object[]> result = memoRepository.getMemoSummaries(pageId);
-        for (Object[] row : result) {
-            String title = (String) row[0];
-            String category = (String) row[1];
-            String subCategory = (String) row[2];
-
-            memoSummaries.computeIfAbsent(category, k -> new HashMap<>())
-                .computeIfAbsent(subCategory, k -> new ArrayList<>())
-                .add(title);
-        }
-        return new MemoSummariesResponseDto.Res(name, memoSummaries);
-    }
+    Map<String, Map<String, List<String>>> memoSummaries = memoRepository.getMemoSummaries(pageId)
+        .stream().collect(
+            Collectors.groupingBy(MemoSummariesView::getCategory,
+                Collectors.groupingBy(MemoSummariesView::getSubCategory,
+                    Collectors.mapping(MemoSummariesView::getTitle, Collectors.toList()))));
+    return new MemoSummariesResponseDto(name, memoSummaries);
+  }
 
 }

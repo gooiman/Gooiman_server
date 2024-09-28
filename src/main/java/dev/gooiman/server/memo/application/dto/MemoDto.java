28818698fa3926bad695d55430ca11cda7bac436
Hyomin Koo <me@eatsteak.dev>
2024-09-28 17:22:01 +0900

    feat: `MemoDto.fromEntity`

--- src/main/java/dev/gooiman/server/memo/application/dto/MemoDto.java
+++ src/main/java/dev/gooiman/server/memo/application/dto/MemoDto.java
@@ -3,13 +3,27 @@ package dev.gooiman.server.memo.application.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import dev.gooiman.server.memo.repository.entity.Memo;
+import java.util.UUID;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MemoDto(
-    String id,
+    UUID id,
     String title,
     String content,
     String author,
     String category,
     String subCategory
-) {}
+) {
+
+  public static MemoDto fromEntity(Memo memo) {
+    return new MemoDto(
+        memo.getMemoId(),
+        memo.getTitle(),
+        memo.getContent(),
+        memo.getUser().getName(),
+        memo.getCategory(),
+        memo.getSubCategory()
+    );
+  }
+}

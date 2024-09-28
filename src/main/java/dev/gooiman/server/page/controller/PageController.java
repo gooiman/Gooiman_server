d38df93b8fea67656c3e873f07ed7f10a920a4e0
Hyomin Koo <me@eatsteak.dev>
2024-09-29 01:00:37 +0900

    refactor: 명시적 리턴 타입

--- src/main/java/dev/gooiman/server/page/controller/PageController.java
+++ src/main/java/dev/gooiman/server/page/controller/PageController.java
@@ -22,19 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Page", description = "페이지 처리 API")
 public class PageController {
 
-    private final PageService pageService;
+  private final PageService pageService;
 
-    @PutMapping
-    @Operation(summary = "페이지 생성", description = "페이지를 생성합니다.")
-    public ResponseDto<CommonIdResponseDto> createPage(
-        @RequestBody CreatePageRequestDto createPageDto) {
-        return ResponseDto.ok(pageService.create(createPageDto));
-    }
+  @PutMapping
+  @Operation(summary = "페이지 생성", description = "페이지를 생성합니다.")
+  public ResponseDto<CommonIdResponseDto> createPage(
+      @RequestBody CreatePageRequestDto createPageDto) {
+    return ResponseDto.ok(pageService.create(createPageDto));
+  }
 
-    @GetMapping("/{pageId}")
-    @Operation(summary = "페이지 정보 조회", description = "페이지 정보를 조회합니다. 주제 및 소주제로 그룹화된 메모 제목이 같이 제공됩니다.")
-    public ResponseDto getPageSummaries(@PathVariable UUID pageId) {
-        MemoSummariesResponseDto.Res res = pageService.memoSummaries(pageId);
-        return ResponseDto.ok(res);
-    }
+  @GetMapping("/{pageId}")
+  @Operation(summary = "페이지 정보 조회", description = "페이지 정보를 조회합니다. 주제 및 소주제로 그룹화된 메모 제목이 같이 제공됩니다.")
+  public ResponseDto<MemoSummariesResponseDto> getPageSummaries(@PathVariable UUID pageId) {
+    MemoSummariesResponseDto res = pageService.memoSummaries(pageId);
+    return ResponseDto.ok(res);
+  }
 }

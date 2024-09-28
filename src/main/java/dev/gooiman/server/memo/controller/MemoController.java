fc70352f16eb8a48562b055d26ce7843f3076faf
Hyomin Koo <me@eatsteak.dev>
2024-09-28 16:09:20 +0900

    feat: `listMemo` endpoint 추가

--- src/main/java/dev/gooiman/server/memo/controller/MemoController.java
+++ src/main/java/dev/gooiman/server/memo/controller/MemoController.java
@@ -1,8 +1,13 @@
 package dev.gooiman.server.memo.controller;
 
+import dev.gooiman.server.common.dto.ResponseDto;
 import dev.gooiman.server.memo.application.MemoService;
+import dev.gooiman.server.memo.application.dto.MemoDto;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemoController {
 
     private final MemoService memoService;
+
+
+    @GetMapping("/")
+    public ResponseDto<MemoDto[]> listMemo(@RequestParam("page_id") UUID pageId, @RequestParam(required = false) String category) {
+        return ResponseDto.ok(memoService.listMemo(pageId, category));
+    }
 
 //    @PutMapping("/memo")
 //    public ResponseEntity<ResponseDto> createMemo(

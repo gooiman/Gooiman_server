43e7d50b1a868d2f9e0162913ec5d70703e3fcc3
koosco <rnxogud123@naver.com>
2024-09-29 02:33:30 +0900

    refactor: repository 불필요한 애노테이션 삭제
    * @Repository 삭제

--- src/main/java/dev/gooiman/server/memo/repository/MemoRepository.java
+++ src/main/java/dev/gooiman/server/memo/repository/MemoRepository.java
@@ -1,23 +1,20 @@
 package dev.gooiman.server.memo.repository;
 
 import dev.gooiman.server.memo.repository.entity.Memo;
-
 import dev.gooiman.server.memo.repository.view.MemoSummariesView;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemoRepository extends JpaRepository<Memo, UUID> {
 
-  List<Memo> findMemosByPage_PageIdAndCategory(UUID pageId, String category);
+    List<Memo> findMemosByPage_PageIdAndCategory(UUID pageId, String category);
 
-  List<Memo> findMemosByPage_PageId(UUID pageId);
+    List<Memo> findMemosByPage_PageId(UUID pageId);
 
-  @Query("select m.title, m.category, m.subCategory from Memo m where m.page.pageId = :pageId")
-  List<MemoSummariesView> getMemoSummaries(@Param("pageId") UUID pageId);
+    @Query("select m.title, m.category, m.subCategory from Memo m where m.page.pageId = :pageId")
+    List<MemoSummariesView> getMemoSummaries(@Param("pageId") UUID pageId);
 
 }
--- src/main/java/dev/gooiman/server/user/repository/UserRepository.java
+++ src/main/java/dev/gooiman/server/user/repository/UserRepository.java
@@ -6,9 +6,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query("select u from User u"

be509a39c0a97cead8e55c871cd89b8be1ddd230
koosco <rnxogud123@naver.com>
2024-09-28 22:14:50 +0900

    fix: MemoRequest에서 uuid로 값 가져오도록 수정

--- src/main/java/dev/gooiman/server/memo/application/dto/CreateMemoRequestDto.java
+++ src/main/java/dev/gooiman/server/memo/application/dto/CreateMemoRequestDto.java
@@ -1,8 +1,9 @@
 package dev.gooiman.server.memo.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.UUID;
 
-public record CreateMemoRequestDto(@JsonProperty(value = "page_id") String pageId,
+public record CreateMemoRequestDto(@JsonProperty(value = "page_id") UUID pageId,
                                    String author,
                                    String title,
                                    String category,
